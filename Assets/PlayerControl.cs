8f85fc5f955b62a89f3f28d0c2fb8f099f65dcb5
Karina <karina.popp@nyu.edu>
2023-02-13 12:24:03 -0500

    adding jumping

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -10,6 +10,15 @@ public class PlayerControl : MonoBehaviour
 
     Rigidbody2D myBody;
 
+    bool grounded = false;
+
+    public float castDist = 0.2f;
+    public float gravityScale = 5f;
+    public float gravityFall = 40f;
+    public float jumpLimit = 2f;
+
+    bool jump = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +29,43 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal");
-        //Debug.Log(horizontalMove);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
     {
         float moveSpeed = horizontalMove * speed;
+
+        if (jump)
+        {
+            myBody.AddForce(Vector2.up * jumpLimit, ForceMode2D.Impulse);
+            jump = false;
+        }
+
+        if(myBody.velocity.y > 0)
+        {
+            myBody.gravityScale = gravityScale;
+        } else if(myBody.velocity.y < 0)
+        {
+            myBody.gravityScale = gravityFall;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist);
+        Debug.DrawRay(transform.position, Vector2.down * castDist, Color.red);
+
+        if(hit.collider != null && hit.transform.name == "Ground")
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
         myBody.velocity = new Vector3(moveSpeed, myBody.velocity.y, 0);
     }
 }
